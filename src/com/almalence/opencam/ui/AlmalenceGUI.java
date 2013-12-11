f02aa487d10ccdbc718bc9d0fef41de68922167c
sergey.malykhin <sergey.malykhin@alamlence.com>
2013-12-11 14:56:45 +0700

    Fixed sale icon setup.

--- src/com/almalence/opencam/ui/AlmalenceGUI.java
+++ src/com/almalence/opencam/ui/AlmalenceGUI.java
@@ -976,7 +976,7 @@ public class AlmalenceGUI extends GUI implements
 	
 	public void ShowGrayUnlockControl()
 	{
-		final RotateImageView unlock = ((RotateImageView) guiView.findViewById(MainScreen.thiz.bOnSale?R.drawable.unlock_sale:R.drawable.unlock));
+		final RotateImageView unlock = ((RotateImageView) guiView.findViewById(R.id.Unlock));
 		if (unlock.getVisibility() == View.VISIBLE)
 			return;
 		unlock.setImageResource(R.drawable.unlock_gray);
