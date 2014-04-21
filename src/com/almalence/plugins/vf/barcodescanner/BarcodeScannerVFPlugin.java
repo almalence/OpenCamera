44c1cd636b7a5396a4354ea59f1bb386b49cca17
Metelskiy Maxim <maxmet91@gmail.com>
2014-04-22 00:20:34 +0700

    Barcode scanner dialogs

--- src/com/almalence/plugins/vf/barcodescanner/BarcodeScannerVFPlugin.java
+++ src/com/almalence/plugins/vf/barcodescanner/BarcodeScannerVFPlugin.java
@@ -3,9 +3,11 @@ package com.almalence.plugins.vf.barcodescanner;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.content.ComponentName;
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
-import android.content.Intent;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnDismissListener;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.graphics.Canvas;
@@ -14,13 +16,24 @@ import android.graphics.Rect;
 import android.hardware.Camera;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
+import android.text.method.LinkMovementMethod;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.almalence.opencam.MainScreen;
 import com.almalence.opencam.Plugin;
@@ -39,11 +52,12 @@ public class BarcodeScannerVFPlugin extends PluginViewfinder {
     
 	private static final double BOUNDS_FRACTION = 0.6;
 	BoundingView mBound = null;
-	private final static int ON = 0;
-	private final static int OFF = 1;
+	private final static Boolean ON = true;
+	private final static Boolean OFF = false;
 	private final MultiFormatReader mMultiFormatReader = new MultiFormatReader();
-	private SoundPlayer mDecodedPlayer = null;
-	public static int mBarcodeScannerState = OFF;
+	private SoundPlayer mSoundPlayer = null;
+	BarcodeArrayAdapter mAdapter = null;
+	public static Boolean mBarcodeScannerState = OFF;
 	private int mFrameCounter = 0;
 	
 	private RotateImageView mBarcodesListButton;
@@ -61,29 +75,30 @@ public class BarcodeScannerVFPlugin extends PluginViewfinder {
 
 	@Override
 	public void onCreate() {
-		UpdatePreferences();
-		this.mBound = new BoundingView(MainScreen.mainContext);
+		mBound = new BoundingView(MainScreen.mainContext);
 		
 		clearViews();
-		addView(this.mBound, ViewfinderZone.VIEWFINDER_ZONE_FULLSCREEN);
+		addView(mBound, ViewfinderZone.VIEWFINDER_ZONE_FULLSCREEN);
+	}
+	
+	@Override
+	public void onResume() {
+		UpdatePreferences();
 	}
 	
 	void UpdatePreferences()
 	{
 		SharedPreferences prefs = PreferenceManager
 				.getDefaultSharedPreferences(MainScreen.mainContext);
-		mBarcodeScannerState = Integer.parseInt(prefs
-				.getString("PrefBarcodescannerVF", "0"));
+		mBarcodeScannerState = prefs.getBoolean("PrefBarcodescannerVF", false);
 		
-		switch (mBarcodeScannerState)
-        {
-        	case ON:
-        	quickControlIconID = R.drawable.gui_almalence_histogram_rgb;
-        	break;
-        	case OFF:
-        	quickControlIconID = R.drawable.gui_almalence_histogram_luma;
-        	break;
-        }
+		if (mBarcodeScannerState == ON) {
+			quickControlIconID = R.drawable.gui_almalence_histogram_rgb;
+		} else {
+			quickControlIconID = R.drawable.gui_almalence_histogram_luma;
+		}
+		
+        showGUI();
 	}
 	
 	@Override
@@ -93,28 +108,55 @@ public class BarcodeScannerVFPlugin extends PluginViewfinder {
 				.getDefaultSharedPreferences(MainScreen.mainContext);
 		Editor editor = prefs.edit();
 		
-        switch (mBarcodeScannerState)
-        {
-	        case ON:
-	        	quickControlIconID = R.drawable.gui_almalence_histogram_luma;
-	        	editor.putString("PrefBarcodescannerVF", "1");
-	        	break;
-	        case OFF:
-	        	quickControlIconID = R.drawable.gui_almalence_histogram_off;
-	        	editor.putString("PrefBarcodescannerVF", "0");
-	        	break;
-        }
+		if (mBarcodeScannerState == ON) {
+			quickControlIconID = R.drawable.gui_almalence_histogram_luma;
+        	editor.putBoolean("PrefBarcodescannerVF", false);
+		} else {
+			quickControlIconID = R.drawable.gui_almalence_histogram_rgb;
+        	editor.putBoolean("PrefBarcodescannerVF", true);
+		}
         editor.commit();
         
         UpdatePreferences();
-        
-        if (mBarcodeScannerState == OFF) {
-			this.mBound.setVisibility(View.GONE);
+	}
+	
+	public void showGUI () {
+		if (mBarcodeScannerState == ON) {
+			if (mBound != null) {
+				mBound.setVisibility(View.VISIBLE);
+			}
+			if (mBarcodesListButton != null) {
+				mBarcodesListButton.setVisibility(View.VISIBLE);
+			}
 		} else {
-			this.mBound.setVisibility(View.VISIBLE);
+			if (mBound != null) {
+				mBound.setVisibility(View.GONE);
+			}
+			if (mBarcodesListButton != null) {
+				mBarcodesListButton.setVisibility(View.GONE);
+			}
 		}
-        
-        UpdatePreferences();
+	}
+	
+	public void initializeSoundPlayers() {
+        mSoundPlayer = new SoundPlayer(MainScreen.mainContext, MainScreen.mainContext.getResources().openRawResourceFd(R.raw.plugin_vf_focus_ok));
+    }
+	
+	public void releaseSoundPlayer() {
+        if (mSoundPlayer != null) {
+        	mSoundPlayer.release();
+        	mSoundPlayer = null;
+        }
+    }
+	
+	@Override
+	public void onCameraParametersSetup() {
+		initializeSoundPlayers();
+	}
+	
+	@Override
+	public void onPause() {
+		releaseSoundPlayer();
 	}
 	
 	@Override
@@ -123,12 +165,13 @@ public class BarcodeScannerVFPlugin extends PluginViewfinder {
     	if (null==camera)
     		return;
 		
-		if (this.mBound == null)
-			this.mBound = new BoundingView(MainScreen.mainContext);
+		if (mBound == null)
+			mBound = new BoundingView(MainScreen.mainContext);
 
 		clearViews();
-		addView(this.mBound, Plugin.ViewfinderZone.VIEWFINDER_ZONE_FULLSCREEN);
+		addView(mBound, Plugin.ViewfinderZone.VIEWFINDER_ZONE_FULLSCREEN);
 		createScreenButton();
+		showGUI();
 	}
 	
 	public void createScreenButton()
@@ -140,7 +183,7 @@ public class BarcodeScannerVFPlugin extends PluginViewfinder {
 		mBarcodesListButton = (RotateImageView) mButtonsLayout.findViewById(R.id.buttonBarcodesList);
 	
 		List<View> specialView = new ArrayList<View>();
-		RelativeLayout specialLayout = (RelativeLayout)MainScreen.thiz.findViewById(R.id.specialPluginsLayout);
+		RelativeLayout specialLayout = (RelativeLayout)MainScreen.thiz.findViewById(R.id.specialPluginsLayout2);
 		for(int i = 0; i < specialLayout.getChildCount(); i++)
 			specialView.add(specialLayout.getChildAt(i));
 
@@ -163,29 +206,59 @@ public class BarcodeScannerVFPlugin extends PluginViewfinder {
 			@Override
 			public void onClick(View v)
 			{
-				Intent intent = new Intent(MainScreen.mainContext, BarcodeListActivity.class);
-				MainScreen.thiz.startActivity(intent);
+				showBarcodesHistoryDialog();
 			}
 			
 		});
 		
 		
-		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-		params.height = (int)MainScreen.thiz.getResources().getDimension(R.dimen.aeawlock_size);
-		params.addRule(RelativeLayout.CENTER_IN_PARENT);
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		params.height = (int)MainScreen.thiz.getResources().getDimension(R.dimen.videobuttons_size) * 4;
+		params.rightMargin = - (int)MainScreen.thiz.getResources().getDimension(R.dimen.videobuttons_size) / 2 ;
+		params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+		params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
 		
-		((RelativeLayout)MainScreen.thiz.findViewById(R.id.specialPluginsLayout)).addView(mButtonsLayout, params);
+		((RelativeLayout)MainScreen.thiz.findViewById(R.id.specialPluginsLayout2)).addView(mButtonsLayout, params);
 		
 		mButtonsLayout.setLayoutParams(params);
 		mButtonsLayout.requestLayout();
 		
-		((RelativeLayout)MainScreen.thiz.findViewById(R.id.specialPluginsLayout)).requestLayout();
+		((RelativeLayout)MainScreen.thiz.findViewById(R.id.specialPluginsLayout2)).requestLayout();
 		
 		mBarcodesListButton.setOrientation(MainScreen.guiManager.getLayoutOrientation());
 		mBarcodesListButton.invalidate();
 		mBarcodesListButton.requestLayout();
 	}
 	
+	protected void showBarcodesHistoryDialog() {
+		final Dialog dialog = new BarcodeHistoryListDialog(MainScreen.thiz);
+
+		ListView barcodesHistoryListView = (ListView) dialog.findViewById(R.id.barcodesHistoryList);
+		mAdapter = new BarcodeArrayAdapter(MainScreen.thiz, BarcodeStorageHelper.getBarcodesList());
+		barcodesHistoryListView.setAdapter(mAdapter);
+		
+		TextView barcodesHistoryEmpty = (TextView) dialog.findViewById(R.id.barcodesHistoryEmpty);
+		if (mAdapter.getCount() > 0) {
+			barcodesHistoryEmpty.setVisibility(View.GONE);
+		}
+		
+		barcodesHistoryListView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				Barcode barcode = mAdapter.getItem(position);
+				showBarcodeViewDialog(barcode);
+			}
+		});
+		dialog.setOnDismissListener(new OnDismissListener() {
+			public void onDismiss(DialogInterface dialog) {
+				mBarcodeScannerState = ON;
+			}
+		});
+		mBarcodeScannerState = OFF;
+
+		dialog.show();
+	}
+
 	@Override
 	public void onPreviewFrame(byte[] data, Camera paramCamera) {
 		if (mBarcodeScannerState == OFF)
@@ -212,55 +285,51 @@ public class BarcodeScannerVFPlugin extends PluginViewfinder {
                 boundingRect.width(), boundingRect.height(), false);
     }
 	
-	/**
-	 * Asynchronous task for decoding and finding barcode
-	 */
-	private class DecodeAsyncTask extends AsyncTask<byte[], Void, Result> {
-	    private int width;
-	    private int height;
-	    
-	    private DecodeAsyncTask(int width, int height) {
-	        this.width = width;
-	        this.height = height;
-	    }
-	    
-	    @Override
-	    protected Result doInBackground(byte[]... datas) {
-	        Result rawResult = null;
-	        final PlanarYUVLuminanceSource source = buildLuminanceSource(datas[0], width,
-                            height, getBoundingRect());
-	        if (source != null) {
-	            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
-	            try {
-	                rawResult = mMultiFormatReader.decodeWithState(bitmap);
-	            } catch (ReaderException re) {
-	                // nothing to do here
-	            } finally {
-	                mMultiFormatReader.reset();
-	            }
-	        }
-	        return rawResult;
-	    }
-	    
-	    @Override
-        protected void onPostExecute(Result result) {
-            if (result != null) {
-                onDecoded(result);
-            }
-        }
-	}
-	
 	public void onDecoded(Result result) {
-        BarcodeStorageHelper.addBarcode(new Barcode(result));
+		Barcode barcode = new Barcode(result);
+        BarcodeStorageHelper.addBarcode(barcode);
         
-		Toast.makeText(MainScreen.mainContext, result.toString(), Toast.LENGTH_SHORT).show();
+        showBarcodeViewDialog(barcode);
         
-        if (mDecodedPlayer == null) {
-        	mDecodedPlayer = new SoundPlayer(MainScreen.mainContext, MainScreen.mainContext.getResources().openRawResourceFd(R.raw.plugin_vf_focus_ok));
-        }
-        if (mDecodedPlayer != null) {
-        	mDecodedPlayer.play();	
-        }
+        if (mSoundPlayer != null)                
+        	if (!MainScreen.ShutterPreference)
+        		mSoundPlayer.play();
+    }
+	
+	protected void showBarcodeViewDialog(Barcode barcode) {
+    	final Dialog dialog = new Dialog(MainScreen.thiz);
+    	dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		dialog.setContentView(R.layout.plugin_vf_barcodescanner_view_layout);
+
+		// set the custom dialog components - text, image and button
+		TextView dataTextView = (TextView) dialog.findViewById(R.id.dataTextView);
+		dataTextView.setMovementMethod(LinkMovementMethod.getInstance());
+		TextView formatTextView = (TextView) dialog.findViewById(R.id.formatTextView);
+		TextView typeTextView = (TextView) dialog.findViewById(R.id.typeTextView);
+		TextView timeTextView = (TextView) dialog.findViewById(R.id.timeTextView);
+		
+		dataTextView.setText(barcode.getData());
+		formatTextView.setText(barcode.getFormat());
+		typeTextView.setText(barcode.getType());
+		timeTextView.setText(barcode.getDate().toString());
+
+		
+		Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
+		dialogButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog.dismiss();
+			}
+		});
+		
+		dialog.setOnDismissListener(new OnDismissListener() {
+			public void onDismiss(DialogInterface dialog) {
+				mBarcodeScannerState = ON;
+			}
+		});
+		
+		mBarcodeScannerState = OFF;
+		dialog.show();
     }
 	
 	/**
@@ -306,6 +375,44 @@ public class BarcodeScannerVFPlugin extends PluginViewfinder {
     }
     
     /**
+	 * Asynchronous task for decoding and finding barcode
+	 */
+	private class DecodeAsyncTask extends AsyncTask<byte[], Void, Result> {
+	    private int width;
+	    private int height;
+	    
+	    private DecodeAsyncTask(int width, int height) {
+	        this.width = width;
+	        this.height = height;
+	    }
+	    
+	    @Override
+	    protected Result doInBackground(byte[]... datas) {
+	        Result rawResult = null;
+	        final PlanarYUVLuminanceSource source = buildLuminanceSource(datas[0], width,
+                            height, getBoundingRect());
+	        if (source != null) {
+	            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+	            try {
+	                rawResult = mMultiFormatReader.decodeWithState(bitmap);
+	            } catch (ReaderException re) {
+	                // nothing to do here
+	            } finally {
+	                mMultiFormatReader.reset();
+	            }
+	        }
+	        return rawResult;
+	    }
+	    
+	    @Override
+        protected void onPostExecute(Result result) {
+            if (result != null) {
+                onDecoded(result);
+            }
+        }
+	}
+    
+    /**
      * View for displaying bounds for active camera region
      */
     class BoundingView extends View {
@@ -322,5 +429,40 @@ public class BarcodeScannerVFPlugin extends PluginViewfinder {
             super.onDraw(canvas);
         }
     }
+    
+    public class BarcodeHistoryListDialog extends Dialog {
+    	Context mainContext;
+    	ListView list;
+    	public BarcodeHistoryListDialog(Context context) {
+		    super(context);
+		    requestWindowFeature(Window.FEATURE_NO_TITLE);
+		    setContentView(R.layout.plugin_vf_barcodescanner_list_layout);
+		    mainContext = context;
+		    list = (ListView) findViewById(R.id.barcodesHistoryList);
+		    registerForContextMenu(list);
+		}
+		
+		@Override
+		public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+			super.onCreateContextMenu(menu, v, menuInfo);
+	        MenuInflater inflater = ((Activity) mainContext).getMenuInflater();
+	        inflater.inflate(R.menu.context_menu_plugin_vf_barcodescanner, menu);
+	        
+	        menu.getItem(0).setOnMenuItemClickListener(new OnMenuItemClickListener()
+	        {
+	            public boolean onMenuItemClick(MenuItem item) {
+	            	AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+	                Barcode barcode = mAdapter.getItem(info.position);
+	                BarcodeStorageHelper.removeBarcode(barcode);
+	                mAdapter.notifyDataSetChanged();
+	                
+	                TextView barcodesHistoryEmpty = (TextView) findViewById(R.id.barcodesHistoryEmpty);
+	        		if (mAdapter.getCount() == 0) {
+	        			barcodesHistoryEmpty.setVisibility(View.VISIBLE);
+	        		}
+	                return true;
+	            }
+	        });
+		}
+    }
 }
-
