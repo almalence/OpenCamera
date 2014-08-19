e6acfea71fffd5fdef44f3afcaf51c5f8f548c90
sergey.malykhin <sergey.malykhin@alamlence.com>
2014-08-19 15:06:38 +0700

    missed import added

--- src/com/almalence/opencam/ui/AlmalenceStore.java
+++ src/com/almalence/opencam/ui/AlmalenceStore.java
@@ -41,6 +41,7 @@ import com.almalence.opencam.R;
 import com.almalence.ui.RotateImageView;
 
 import android.content.SharedPreferences;
+import android.os.CountDownTimer;
 import android.preference.PreferenceManager;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
