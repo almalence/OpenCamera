f81939740b703baa0c71609c5701e77576133c00
Metelskiy Maxim <maxmet91@gmail.com>
2014-12-17 13:18:47 +0700

    SoundPlayer fix

--- src/com/almalence/opencam/SoundPlayer.java
+++ src/com/almalence/opencam/SoundPlayer.java
@@ -26,147 +26,79 @@ package com.almalence.opencam;
 
 import java.io.IOException;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.AssetFileDescriptor;
+import android.media.AudioAttributes;
 import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.util.Log;
+import android.media.SoundPool;
 
 /**
  * Plays an AssetFileDescriptor, but does all the hard work on another thread so
  * that any slowness with preparing or loading doesn't block the calling thread.
  */
-public class SoundPlayer implements Runnable
-{
-	private static final String	TAG			= "SoundPlayer";
-	private Thread				mThread;
-	private MediaPlayer			mPlayer;
-	private int					mPlayCount	= 0;
-	private boolean				mExit;
-	private AssetFileDescriptor	mAfd;
-	private int					mAudioStreamType;
-	private Context				mContext;
+public class SoundPlayer {
+	private static final String TAG = "SoundPlayer";
+	private SoundPool mPlayer;
+	private int soundStartId = 0;
+	private AssetFileDescriptor mAfd;
+	private int mAudioStreamType;
+	AudioManager audio;
 
-	@Override
-	public void run()
-	{
-		while (true)
-		{
-			try
-			{
-				AudioManager audio = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
-				if (mPlayer == null)
-				{
-					MediaPlayer player = new MediaPlayer();
-					player.setAudioStreamType(mAudioStreamType);
-					player.setDataSource(mAfd.getFileDescriptor(), mAfd.getStartOffset(), mAfd.getLength());
-					player.setLooping(false);
-					player.prepare();
-					mPlayer = player;
-					mAfd.close();
-					mAfd = null;
-				}
-				synchronized (this)
-				{
-					while (true)
-					{
-						if (mExit)
-						{
-							return;
-						} else if (mPlayCount <= 0)
-						{
-							wait();
-						} else
-						{
-							mPlayCount--;
-							break;
-						}
-					}
-				}
-				switch (audio.getRingerMode())
-				{
-				case AudioManager.RINGER_MODE_NORMAL:
-					mPlayer.start();
-					break;
-				case AudioManager.RINGER_MODE_SILENT:
-					break;
-				case AudioManager.RINGER_MODE_VIBRATE:
-					break;
-				default:
-					break;
-				}
-			} catch (Exception e)
-			{
-				Log.e(TAG, "Error playing sound", e);
+	@SuppressLint("NewApi")
+	public SoundPlayer(Context mContext, AssetFileDescriptor afd1) {
+		mAfd = afd1;
+		mAudioStreamType = AudioManager.STREAM_SYSTEM;
+
+		audio = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+		if (mPlayer == null) {
+			if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+				mPlayer = new SoundPool.Builder()
+				.setMaxStreams(5)
+				.setAudioAttributes(
+						new AudioAttributes.Builder()
+						.setLegacyStreamType(mAudioStreamType)
+						.setContentType(
+								AudioAttributes.CONTENT_TYPE_SONIFICATION)
+								.build()).build();
+			} else {
+				mPlayer = new SoundPool(5, mAudioStreamType, 0);
 			}
-		}
-	}
-
-	public SoundPlayer(Context mContext, AssetFileDescriptor afd)
-	{
-		this.mContext = mContext;
-
-		mAfd = afd;
-		mAudioStreamType = AudioManager.STREAM_NOTIFICATION;
-	}
-
-	public SoundPlayer(Context mContext, AssetFileDescriptor afd, boolean enforceAudible)
-	{
-		this.mContext = mContext;
-
-		mAfd = afd;
-		if (enforceAudible)
-		{
-			mAudioStreamType = 7;
-		} else
-		{
-			mAudioStreamType = AudioManager.STREAM_NOTIFICATION;
-		}
-	}
-
-	public void play()
-	{
-		if (mThread == null)
-		{
-			mThread = new Thread(this);
-			mThread.start();
-		}
-		synchronized (this)
-		{
-			mPlayCount++;
-			notifyAll();
-		}
-	}
-
-	public void release()
-	{
-		if (mThread != null)
-		{
-			synchronized (this)
-			{
-				mExit = true;
-				notifyAll();
-			}
-			try
-			{
-				mThread.join();
-			} catch (InterruptedException e)
-			{
-			}
-			mThread = null;
-		}
-		if (mAfd != null)
-		{
-			try
-			{
+			
+			soundStartId = mPlayer.load(mAfd, 1);
+			try {
 				mAfd.close();
-			} catch (IOException e)
-			{
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 			mAfd = null;
 		}
-		if (mPlayer != null)
-		{
+	}
+
+	public void play() {
+		switch (audio.getRingerMode()) {
+		case AudioManager.RINGER_MODE_NORMAL:
+			mPlayer.play(soundStartId, 1, 1, 0, 0, 1);
+			break;
+		case AudioManager.RINGER_MODE_SILENT:
+			break;
+		case AudioManager.RINGER_MODE_VIBRATE:
+			break;
+		default:
+			break;
+		}
+	}
+	
+	public void release() {
+		if (mAfd != null) {
+			try {
+				mAfd.close();
+			} catch (IOException e) {
+			}
+			mAfd = null;
+		}
+		if (mPlayer != null) {
 			mPlayer.release();
 			mPlayer = null;
 		}
