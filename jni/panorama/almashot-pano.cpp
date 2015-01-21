0b32c8d46b642b2e1be3abe1bed1f77969c28aea
sergey.malykhin <sergey.malykhin@alamlence.com>
2015-01-21 19:06:23 +0600

    Fixes for debug.

--- jni/panorama/almashot-pano.cpp
+++ jni/panorama/almashot-pano.cpp
@@ -89,6 +89,91 @@ extern "C" JNIEXPORT jint JNICALL Java_com_almalence_plugins_processing_panorama
 	return result;
 }
 
+#include <sys/stat.h>
+#include <dirent.h>
+
+static int nextdirnum(const char *path) {
+	DIR *dir = opendir(path);
+	int max = -1;
+	if (dir) {
+		struct dirent *ent;
+		while ((ent = readdir(dir))) {
+			char c;	int a;
+			if (sscanf(ent->d_name, "%i%c", &a, &c) == 1)
+				if (max < a) max = a;	
+		}
+		closedir(dir);
+	}
+	return max + 1;
+}
+
+#include "../libjpeg/jpeglib.h"
+
+static int write_jpeg(void *image, void *imgUV, int stride,
+		int width, int height, const char *fn, int quality) {
+  struct jpeg_compress_struct cinfo;
+  struct jpeg_error_mgr jerr;
+  FILE *fo; int i, wa = (width+15)&-16;
+	JSAMPROW ptrY[16], ptrU[8], ptrV[8];
+	JSAMPARRAY data[3] = { ptrY, ptrU, ptrV };
+	JSAMPROW buf;
+
+	if (!imgUV) imgUV = (char*)image + width * height, stride = width;
+
+	if ((width | height) & 1) return 0;
+	if (!(buf = (JSAMPROW)malloc(24 * wa))) return 0;
+  if (!(fo = fopen(fn, "wb"))) { free(buf); return 0; }
+
+  cinfo.err = jpeg_std_error(&jerr);
+  jpeg_create_compress(&cinfo);
+  jpeg_stdio_dest(&cinfo, fo);
+
+  cinfo.image_width = width;
+  cinfo.image_height = height;
+  cinfo.input_components = 3;
+  jpeg_set_defaults(&cinfo);
+	// jpeg_set_colorspace(&cinfo, JCS_YCbCr); 
+  cinfo.in_color_space = JCS_YCbCr;
+	cinfo.raw_data_in = TRUE;
+	cinfo.comp_info[0].h_samp_factor = 2;
+	cinfo.comp_info[0].v_samp_factor = 2;
+	cinfo.comp_info[1].h_samp_factor = 1;
+	cinfo.comp_info[1].v_samp_factor = 1;
+	cinfo.comp_info[2].h_samp_factor = 1;
+	cinfo.comp_info[2].v_samp_factor = 1;
+
+  jpeg_set_quality(&cinfo, quality, TRUE /* limit to baseline-JPEG values */);
+  jpeg_start_compress(&cinfo, TRUE);
+
+	for (i = 0; i < 16; i++) ptrY[i] = buf + i * wa;
+	for (i = 0; i < 8; i++)
+		ptrV[i] = (ptrU[i] = buf + (i + 16) * wa) + (wa >> 1);
+
+	for (i = 0; i < height; i += 16) {
+		int j, n = (height - i);
+		if (n > 16) n = 16;
+		for (j = 0; j < n; j++) {
+			memcpy(ptrY[j], (JSAMPROW)image + (i + j) * stride, width);
+			if (width < wa) memset(ptrY[j]+width, ptrY[j][width-1], wa - width);
+			if (j&1) {
+				int k; JSAMPROW d = ptrU[j>>1], s = (JSAMPROW)imgUV + ((i+j)>>1) * stride;
+				for (k = 0; k < (width>>1); k++) d[k] = s[k*2+1], d[k + (wa>>1)] = s[k*2];
+				for ( ; k < (wa>>1); k++) d[k] = d[(width>>1)-1], d[k + (wa>>1)] = d[(wa>>1) + (width>>1)-1];
+			}
+		}
+		for ( ; j < 15; j += 2) {
+			ptrY[j] = ptrY[n-1]; ptrY[j+1] = ptrY[n-1];
+			ptrU[j>>1] = ptrU[(n>>1)-1]; ptrV[j>>1] = ptrV[(n>>1)-1];
+		}
+		jpeg_write_raw_data(&cinfo, data, 16);
+  }
+  jpeg_finish_compress(&cinfo);
+  jpeg_destroy_compress(&cinfo);
+  fclose(fo);
+	free(buf);
+	return 1;
+}
+
 extern "C" JNIEXPORT jintArray JNICALL Java_com_almalence_plugins_processing_panorama_AlmashotPanorama_process
 (
 	JNIEnv* env,
@@ -156,9 +241,18 @@ extern "C" JNIEXPORT jintArray JNICALL Java_com_almalence_plugins_processing_pan
 	//		width, height);
 
 #if 0 // debug dump
+	char path[128], *fn;
 	{
 		int i;
-		FILE *fo = fopen("/sdcard/pano/pano.txt", "wb");
+
+		mkdir("/sdcard/pano", 0770);
+		i = nextdirnum("/sdcard/pano");
+		fn = path + sprintf(path, "/sdcard/pano/%i", i);
+		mkdir(path, 0770);
+		*fn++ = '/';
+		strcpy(fn, "pano.txt");
+
+		FILE *fo = fopen(path, "wb");
 		if (fo) {
 			fprintf(fo, "#pano (w = %i, h = %i, n = %i, fov = %i, overlap = %.2f)\n\n",
 					width, height, nframesCount, cameraFOV, intersection);
@@ -173,14 +267,14 @@ extern "C" JNIEXPORT jintArray JNICALL Java_com_almalence_plugins_processing_pan
 		}
 
 		for (i = 0; i < nframesCount; i++) {
-			char str[64];
-			sprintf(str, "/sdcard/pano/pano%d.yuv", i);
-			fo = fopen(str, "wb");
+			sprintf(fn, "pano%i.yuv", i);
+			fo = fopen(path, "wb");
 			if (fo) {
-				fwrite((Uint8*)nframes[i], 1, width*height*3/2, fo);
+				fwrite((void*)nframes[i], 1, width*height*3/2, fo);
 				fclose(fo);
 			}
 		}
+		strcpy(fn, "pano.jpg");
 	}
 #endif
 
@@ -204,6 +298,21 @@ extern "C" JNIEXPORT jintArray JNICALL Java_com_almalence_plugins_processing_pan
 
 	out = NULL;
 	status = Pano_Process(instance, &out);
+
+#if 0 // result for debug dump
+	write_jpeg(out, 0, 0, out_width, out_height, path, 100);
+	if (0) {
+		strcpy(fn, "crop.jpg");
+		unsigned char *outUV = out + out_width * out_height;
+		int st_x = crop[0], st_y = crop[1], width = crop[2], height = crop[3];
+		width = (width + (st_x & 1) + 1) & -2; st_x &= -2;
+		height = (height + (st_y & 1) + 1) & -2; st_y &= -2;
+		write_jpeg(
+			out + st_y * out_width + st_x,
+			outUV + (st_y>>1) * out_width + st_x,
+			out_width, width, height, path, 100);
+	}
+#endif
 
 	//FILE* f = fopen("/sdcard/output.yuv", "wb");
 	//fwrite(out, out_width * out_height * 3 / 2, 1, f);
