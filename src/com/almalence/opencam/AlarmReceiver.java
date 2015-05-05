e05398c44b9a8840527f91dd9c8907658a0cf2ae
sergey.malykhin <sergey.malykhin@alamlence.com>
2015-05-05 14:37:44 +0600

    Minor fix for alarmreceiver.

--- src/com/almalence/opencam/AlarmReceiver.java
+++ src/com/almalence/opencam/AlarmReceiver.java
@@ -158,7 +158,7 @@ public class AlarmReceiver extends BroadcastReceiver
 		pm.setComponentEnabledSetting(receiver, PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
 				PackageManager.DONT_KILL_APP);
 
-		if (wakeLock == null)
+		if (wakeLock != null)
 			if (wakeLock.isHeld())
 				wakeLock.release();
 	}
